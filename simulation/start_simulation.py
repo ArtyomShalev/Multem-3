3ba69064065e7ba8f651536b960058d688af19ca
ashalev <artyomshalev@gmail.com>
2023-08-18 13:39:59 +0300

    first simulation was added

--- simulation/start_simulation.py
+++ simulation/start_simulation.py
@@ -1,19 +1,22 @@
 '''
 this script is used to run and test Multem-3 simulation 
 there are different options how to run Multem-3:
+! important: multem3 binary file should be in the directory with fort.10 and mutipole_regime_prameters.ini
 --------------------------------------------------------
 1.  manually build this project: run cmake and make
     copy fort.10 to directory Multem-3/bin
     run multem3 bin file (Multem-3/bin)
 
 2.  manually build this project: run cmake and make
-    run this script
+    run this script 
 
 3. run run_multem3.sh script
 --------------------------------------------------------
 '''
 import time
 import numpy as np
+import sys
+sys.path.insert(0, '..')
 import src.python_simulations.multem3_py_calculating as calc
 
 
@@ -46,6 +49,6 @@ omega = np.linspace(3.7275, 3.74, 300)/2/np.pi
 kx = np.linspace(0, 0.5, 100)
 time0 = time.time()
 F, T, R, A = calc.calc_spectrum_omega(omega, 2, 0.01, input_params)
+print(f'transmittance\n{T}')
 print(f'calculation time: {time.time()-time0}s')
-#TODO print the result
 
