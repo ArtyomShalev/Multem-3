7c15a4407176fecb996ac6b22e5c1a9003560141
ashalev <artyomshalev@gmail.com>
2024-01-11 16:20:38 +0300

    changed desciption a bit

--- simulation/start_simulation.py
+++ simulation/start_simulation.py
@@ -10,7 +10,6 @@ there are different options how to run Multem-3:
 2.  manually build this project: run cmake and make
     run this script 
 
-3. run run_multem3.sh script #TODO create this script
 --------------------------------------------------------
 '''
 import time
